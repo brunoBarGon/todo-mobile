041bc851b05213b98f781d7bb19b61b4cf1a42f0
Diego Leite <diego@domob.me>
2020-10-02 09:25:46 -0300

    chore: navigation

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Dashboard from '../pages/Dashboard'
 import Tarefas from '../pages/Tarefas'
 
-const App = createStackNavigator();
+const App = createBottomTabNavigator();
 
 const AppRoutes = () => {
   return (
-    <App.Navigator screenOptions={{
-      headerShown: false
-    }}>
-      <App.Screen name="Dashboard" component={Dashboard} />
-      <App.Screen name="Tarefas" component={Tarefas} />
+    <App.Navigator 
+      initialRouteName="Dashboard"
+    >
+      <App.Screen 
+        name="Dashboard" 
+        component={Dashboard} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="view-dashboard" color="#000" size={30} />
+          )
+        }}
+      />
+      <App.Screen 
+        name="Tarefas" 
+        component={Tarefas} 
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="playlist-edit" color="#000" size={30} />
+          )
+        }}
+      />
     </App.Navigator>
   )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: "http://10.0.2.2:3333"
-  baseURL: "http://6cc78b87d24d.ngrok.io"
+  baseURL: "http://10.0.2.2:3333"
 });
 
 export default api; 
